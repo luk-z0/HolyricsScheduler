3db83bfab776903e924a265893a3a9d9bc435059
lucasluz <lucasdias200lucasdias@gmail.com>
2025-07-29 03:32:49 -0300

    adding condition to delete previous generated_schedule.txt file in start.sh

--- start.sh
+++ start.sh
@@ -9,6 +9,20 @@ chmod +x "$SCRIPT"
 echo "Baixando input.txt da branch main do repositório..."
 wget -q --show-progress -O input.txt https://raw.githubusercontent.com/luk-z0/HolyricsScheduler/main/input.txt
 
+if [ -f "generated_schedule.txt" ]; then
+    echo "O arquivo generated_schedule.txt existe."
+    read -p "Deseja deletá-lo antes de continuar? (s/n) " response
+    case "$response" in
+        [Ss]* )
+            rm -f generated_schedule.txt
+            echo "Arquivo deletado."
+            ;;
+        * )
+            echo "Arquivo mantido."
+            ;;
+    esac
+fi
+
 python3 "$SCRIPT"
 
 echo "Script executado. Verifique o arquivo gerado."
