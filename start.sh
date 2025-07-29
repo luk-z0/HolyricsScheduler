c97e91ccefad738886cbe0cf92b73ef8c96cd1d2
lucasluz <lucasdias200lucasdias@gmail.com>
2025-07-29 03:25:14 -0300

    feat: download a updated input.txt

--- start.sh
+++ start.sh
@@ -6,6 +6,9 @@ SCRIPT="script_to_generate_schedule.py"
 
 chmod +x "$SCRIPT"
 
+echo "Baixando input.txt da branch main do repositório..."
+wget -q --show-progress -O input.txt https://raw.githubusercontent.com/luk-z0/HolyricsScheduler/main/input.txt
+
 python3 "$SCRIPT"
 
 echo "Script executado. Verifique o arquivo gerado."
